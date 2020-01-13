1e74b19f932339b30f13b362e87b92f988e81667
Praveen Chandra Tiwari <tiwariPC@users.noreply.github.com>
2020-01-13 14:46:30 +0530

    added scripts

---
+++ shell_scripts/crab_step2_dirdel.sh
@@ -0,0 +1,75 @@
+cd  bbDM_2HDMa_MH3_1200_MH4_1000_Mchi_1
+rm -rf crab_projects_step2
+cd ..
+cd  bbDM_2HDMa_MH3_1200_MH4_100_Mchi_1
+rm -rf crab_projects_step2
+cd ..
+cd  bbDM_2HDMa_MH3_1200_MH4_10_Mchi_1
+rm -rf crab_projects_step2
+cd ..
+cd  bbDM_2HDMa_MH3_1200_MH4_150_Mchi_1
+rm -rf crab_projects_step2
+cd ..
+cd  bbDM_2HDMa_MH3_1200_MH4_200_Mchi_1
+rm -rf crab_projects_step2
+cd ..
+cd  bbDM_2HDMa_MH3_1200_MH4_250_Mchi_1
+rm -rf crab_projects_step2
+cd ..
+cd  bbDM_2HDMa_MH3_1200_MH4_300_Mchi_1
+rm -rf crab_projects_step2
+cd ..
+cd  bbDM_2HDMa_MH3_1200_MH4_350_Mchi_1
+rm -rf crab_projects_step2
+cd ..
+cd  bbDM_2HDMa_MH3_1200_MH4_400_Mchi_1
+rm -rf crab_projects_step2
+cd ..
+cd  bbDM_2HDMa_MH3_1200_MH4_450_Mchi_1
+rm -rf crab_projects_step2
+cd ..
+cd  bbDM_2HDMa_MH3_1200_MH4_500_Mchi_1
+rm -rf crab_projects_step2
+cd ..
+cd  bbDM_2HDMa_MH3_1200_MH4_50_Mchi_1
+rm -rf crab_projects_step2
+cd ..
+cd  bbDM_2HDMa_MH3_1200_MH4_700_Mchi_1
+rm -rf crab_projects_step2
+cd ..
+cd  bbDM_2HDMa_MH3_1200_MH4_750_Mchi_1
+rm -rf crab_projects_step2
+cd ..
+cd  bbDM_2HDMa_MH3_600_MH4_100_Mchi_1
+rm -rf crab_projects_step2
+cd ..
+cd  bbDM_2HDMa_MH3_600_MH4_10_Mchi_1
+rm -rf crab_projects_step2
+cd ..
+cd  bbDM_2HDMa_MH3_600_MH4_150_Mchi_1
+rm -rf crab_projects_step2
+cd ..
+cd  bbDM_2HDMa_MH3_600_MH4_200_Mchi_1
+rm -rf crab_projects_step2
+cd ..
+cd  bbDM_2HDMa_MH3_600_MH4_250_Mchi_1
+rm -rf crab_projects_step2
+cd ..
+cd  bbDM_2HDMa_MH3_600_MH4_300_Mchi_1
+rm -rf crab_projects_step2
+cd ..
+cd  bbDM_2HDMa_MH3_600_MH4_350_Mchi_1
+rm -rf crab_projects_step2
+cd ..
+cd  bbDM_2HDMa_MH3_600_MH4_400_Mchi_1
+rm -rf crab_projects_step2
+cd ..
+cd  bbDM_2HDMa_MH3_600_MH4_450_Mchi_1
+rm -rf crab_projects_step2
+cd ..
+cd  bbDM_2HDMa_MH3_600_MH4_500_Mchi_1
+rm -rf crab_projects_step2
+cd ..
+cd  bbDM_2HDMa_MH3_600_MH4_50_Mchi_1
+rm -rf crab_projects_step2
+cd ..
